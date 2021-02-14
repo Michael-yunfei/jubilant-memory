6b7c52f2e6bb954e6259b11f9a2f6142d61ea99e
wangfei1 <wangfei1@sensetime.com>
2021-02-14 12:41:38 +0800

    wrap up the website

--- scripts/main.js
+++ scripts/main.js
@@ -58,3 +58,122 @@ function zodiacfun (choice) {
 //         buttondisplay.innerHTML = "Play"
 //     }
 // }
+
+// when animating on canvas, it is best to use requestAnimationFrame instead of setTimeout or setInterval
+// not supported in all browsers though and sometimes needs a prefix, so we need a shim
+window.requestAnimFrame = ( function() {
+	return window.requestAnimationFrame ||
+				window.webkitRequestAnimationFrame ||
+				window.mozRequestAnimationFrame ||
+				function( callback ) {
+					window.setTimeout( callback, 1000 / 60 );
+				};
+})();
+
+// now we will setup our basic variables for the demo
+
+// setup canvas
+var canvas = document.querySelector("#canvas")
+var ctx = canvas.getContext('2d')
+canvas.width = innerWidth  // crucial part
+canvas.height = innerHeight
+
+const mouse = {
+    x: innerWidth / 2, 
+    y: innerHeight / 2
+}
+
+
+// add gravity
+const gravity = 0.05
+const friction = 0.995
+
+// we creat our fireworks as a class
+class Particle {
+
+    // construct the class
+    constructor(x, y, radius, color, velocity) {
+        this.x = x 
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+        this.alpha = 1
+    }
+
+    draw() {
+        ctx.save()
+        ctx.globalAlpha = this.alpha
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+        ctx.closePath()
+        ctx.restore()
+      }
+
+    // call up draw function
+    update() {
+        this.draw()
+        this.velocity.y += gravity
+        this.velocity.x *= friction
+        this.velocity.y *= friction
+        this.x += this.velocity.x 
+        this.y += this.velocity.y
+        this.alpha -= 0.005
+    }
+}
+
+// now we need initialize this class
+let particles = []
+
+// animation loop
+function animate() {
+    requestAnimationFrame(animate)
+    particles.forEach((particle, i) => {
+        if (particle.alpha > 0) {
+            particle.update()
+        } else {
+            particles.splice(i, 1)
+        }
+    });
+
+}
+
+var  clickcount = 0
+
+const canimation = document.querySelector("#canvas")
+canimation.addEventListener('click', (event) => {
+    mouse.x = event.clientX
+    mouse.y = event.clientY
+
+    clickcount += 1
+
+    // creat a circle
+    const particlecount = 196
+    const angleincrment = Math.PI * 2 / particlecount 
+    // now we creat our particles
+    for (let i = 0; i < particlecount; i++) {
+        particles.push(
+            new Particle(mouse.x, mouse.y, 3.9, `hsl(${Math.random() * 360},
+            50%, 50%)`, {
+                x: Math.cos(angleincrment * i) * Math.random() * 12,
+                y: Math.sin(angleincrment * i) * Math.random() * 12
+            })
+        )
+    }
+
+    if (clickcount == 2) {
+        const chapter1content = document.querySelector(".chapter1content")
+        chapter1content.innerHTML = `
+        <p>
+        Click any place of the area inside box to fire your fireworks.
+        </p>
+        <button onclick="location.href='chapter2.html'" type="button">Next Chapter</button>`
+    }
+})
+
+animate()
+
+
+
